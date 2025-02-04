17853429fa4b1e1a7bb628fb64cc55063b0b6faf
urosnikolic123 <uros.n.nikolic123@gmail.com>
2025-02-04 14:44:26 +0100

    cr changes

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+import aspectRatio from "@tailwindcss/aspect-ratio";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "min-1184": "1184px",
+        "min-1290": "1290px",
+        "min-556": "556px",
+        "min-360": "360px",
+        "min-400": "400px",
+      },
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+        Roboto_Condensed: ["Roboto Condensed", "sans-serif"],
+        Bebas_Neue: ["Bebas Neue"],
+        Space_Grotesk: ["Space Grotesk"],
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        dark: "#303030",
+        blue: "#0081A5",
+        yellow: "#FFC436",
+      },
+    },
+  },
+  plugins: [aspectRatio],
+} satisfies Config;
